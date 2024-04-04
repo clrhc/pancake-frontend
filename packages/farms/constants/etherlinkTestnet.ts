1b7bdccef37301640fe881a9e26e7f0de1106e2b
clrhc <s2celerity@gmail.com>
2024-04-03 19:04:34 -0600

    update opBNBtestnet to etherlinkTestnetTokens

--- packages/farms/constants/etherlinkTestnet.ts
+++ packages/farms/constants/etherlinkTestnet.ts
@@ -8,22 +8,22 @@ export const farmsV3 = defineFarmV3Configs([
   {
     pid: 1,
     lpAddress: '0x5EFf51E8718F7b36D09CBc0506f653E886eBdAdb',
-    token0: opBnbTestnetTokens.ign,
-    token1: opBnbTestnetTokens.wxtz,
+    token0: etherlinkTestnetTokens.ign,
+    token1: etherlinkTestnetTokens.wxtz,
     feeAmount: FeeAmount.LOW,
   },
   {
     pid: 2,
     lpAddress: '0x71C5C28442353877f6a4A196f95Ee4CfB0Dcb5Aa',
-    token0: opBnbTestnetTokens.wxtz,
-    token1: opBnbTestnetTokens.weth,
+    token0: etherlinkTestnetTokens.wxtz,
+    token1: etherlinkTestnetTokens.weth,
     feeAmount: FeeAmount.MEDIUM,
   },
   {
     pid: 3,
     lpAddress: '0x6Cd45Ef02989A9188A9d8B170b86FAAA6d2B194C',
-    token0: opBnbTestnetTokens.ign,
-    token1: opBnbTestnetTokens.usdc,
+    token0: etherlinkTestnetTokens.ign,
+    token1: etherlinkTestnetTokens.usdc,
     feeAmount: FeeAmount.HIGH,
   },
 ])
